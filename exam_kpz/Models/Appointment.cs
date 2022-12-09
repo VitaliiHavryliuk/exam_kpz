2874ee0f1e64575ab243918e8bd7f2d8307c504e
anhelinafetsenets <91602319+anhelinafetsenets@users.noreply.github.com>
2022-12-09 14:50:32 +0200

    Add project files.

---
+++ exam_kpz/Models/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace exam_kpz.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int AppointmentId { get; set; }
+        public DateOnly Date { get; set; }
+
+        [ForeignKey("Patient")]
+        public int PatientId { get; set; }
+
+        public virtual Patient Patient { get; set; }
+    }
+}
---
+++ exam_kpz/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using exam_kpz.Models;
+using exam_kpz;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        
+        builder.Services.AddDbContext<VetContext>(opt => opt.UseInMemoryDatabase("VetList"));
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
